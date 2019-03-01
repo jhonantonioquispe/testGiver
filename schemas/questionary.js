1572e747e1c58a974d603826f0e9c201bacd1c01
jhon quispe <jaqch6887@hotmail.es>
2019-03-01 09:19:10 -0400

    adding sample of questionary

--- schemas/questionary.js
+++ schemas/questionary.js
@@ -35,6 +35,96 @@
   ]
 }*/
 
+/*
+example of questionary
+{
+  "_id" : ObjectId("5c76106a74d45e11dc3ef705"),
+  "totalScore" : 55.34,
+  "author" : "jhon quispe",
+  "type" : "1",
+  "creationDate" : ISODate("2019-02-27T04:22:02.257Z"),
+  "questions" : [ 
+      {
+          "_id" : ObjectId("5c76106a74d45e11dc3ef701"),
+          "questionText" : "esta es una nueva pregunta que ",
+          "options" : [ 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef704"),
+                  "text" : "hola",
+                  "attachment" : "imgUploader_1549941201750_views.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef703"),
+                  "text" : "que",
+                  "attachment" : "imgUploader_1549942722887_georgina-rodriguez-novia-ronaldo-1-1548258125.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef702"),
+                  "text" : "tal",
+                  "attachment" : "imgUploader_1549432553391_images.jpg"
+              }
+          ],
+          "answer" : "_id0"
+      }, 
+      {
+          "_id" : ObjectId("5c76106a74d45e11dc3ef6ff"),
+          "questionText" : "estamos modificando la segunda pregunta hasta ahora tendria que funcionar\n    ",
+          "options" : [ 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef700"),
+                  "text" : "tal",
+                  "attachment" : "imgUploader_1549942781532_imgUploader_1527836764676_hijo05.jpg"
+              }
+          ],
+          "answer" : "_id2"
+      }, 
+      {
+          "_id" : ObjectId("5c76106a74d45e11dc3ef6f7"),
+          "questionText" : "this is the third question with a a lot of images ",
+          "options" : [ 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef6fe"),
+                  "text" : "tal",
+                  "attachment" : "http://localhost:3000/attachments/imgUploader_1527836764676_hijo05.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef6fd"),
+                  "text" : "tal www",
+                  "attachment" : "http://localhost:3000/attachments/imgUploader_1527836764676_hijo05.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef6fc"),
+                  "text" : "tal dfd",
+                  "attachment" : "http://localhost:3000/attachments/imgUploader_1527836764676_hijo05.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef6fb"),
+                  "text" : "tal dfd",
+                  "attachment" : "http://localhost:3000/attachments/imgUploader_1527836764676_hijo05.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef6fa"),
+                  "text" : "tal dfd",
+                  "attachment" : "http://localhost:3000/attachments/imgUploader_1527836764676_hijo05.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef6f9"),
+                  "text" : "tal dfd",
+                  "attachment" : "http://localhost:3000/attachments/imgUploader_1527836764676_hijo05.jpg"
+              }, 
+              {
+                  "_id" : ObjectId("5c76106a74d45e11dc3ef6f8"),
+                  "text" : "talddd",
+                  "attachment" : "http://localhost:3000/attachments/imgUploader_1527836764676_hijo05.jpg"
+              }
+          ],
+          "answer" : "_id1"
+      }
+  ],
+  "__v" : 0
+} 
+*/
+
 const schema = {
   totalScore: { type: Number },
   author: { type: String },
